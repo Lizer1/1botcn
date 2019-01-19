31760107f24b700df575bad17f467e90fd5251c2
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-19 14:25:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,43 +217,42 @@ function play(guild, song) {
 
 
 
-const developers = ["ا447342860769951754"]//Toxic Codes
-client.on('message', message => {//Toxic Codes
-    var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
+const developers = ["447342860769951754"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
   if (message.content.startsWith(adminprefix + '#')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-     if (message.content === (adminprefix + "leave")) {//Toxic Codes
-    message.guild.leave();   //Toxic Codes
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();   
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+      message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/zero");
-      message.channel.send(`**✅**`)//Toxic Codes
+      message.channel.send(`**✅**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-  if (message.content.startsWith(adminprefix + 'setprefix')) {//Toxic Codes
+  if (message.content.startsWith(adminprefix + 'setprefix')) {
   client.user.setPrefix(argresult).then
-      message.channel.send(`Changing Prefix ..**${argresult}** `)//Toxic Codes
+      message.channel.send(`Changing Prefix ..**${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {//Toxic Codes
+if (message.content.startsWith(adminprefix + 'setavatar')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);//Toxic Codes
-}
-});//Toxic Codes
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+});
 
 
 
